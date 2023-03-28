938feea7dcd87ab1244025b45e85fd9802afadc7
Mariana Parducci <100160071+m-parducci@users.noreply.github.com>
2023-03-27 21:48:45 -0300

    Add files via upload

---
+++ Atividade4/Atividade-Triângulo/Atividade-Triângulo/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace Atividade_Triângulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTestar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLadoA = new System.Windows.Forms.TextBox();
+            this.txtLadoB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLadoC = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTestar
+            // 
+            this.btnTestar.AllowDrop = true;
+            this.btnTestar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTestar.Location = new System.Drawing.Point(120, 301);
+            this.btnTestar.Name = "btnTestar";
+            this.btnTestar.Size = new System.Drawing.Size(171, 52);
+            this.btnTestar.TabIndex = 0;
+            this.btnTestar.Text = "Testar";
+            this.btnTestar.UseVisualStyleBackColor = true;
+            this.btnTestar.Click += new System.EventHandler(this.btnTestar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(124, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lado A\r\n";
+            // 
+            // txtLadoA
+            // 
+            this.txtLadoA.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLadoA.Location = new System.Drawing.Point(293, 47);
+            this.txtLadoA.Name = "txtLadoA";
+            this.txtLadoA.Size = new System.Drawing.Size(184, 53);
+            this.txtLadoA.TabIndex = 2;
+            this.txtLadoA.TextChanged += new System.EventHandler(this.txtLadoA_TextChanged);
+            this.txtLadoA.Validating += new System.ComponentModel.CancelEventHandler(this.txtLadoA_Validating);
+            // 
+            // txtLadoB
+            // 
+            this.txtLadoB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLadoB.Location = new System.Drawing.Point(293, 128);
+            this.txtLadoB.Name = "txtLadoB";
+            this.txtLadoB.Size = new System.Drawing.Size(184, 53);
+            this.txtLadoB.TabIndex = 4;
+            this.txtLadoB.Validating += new System.ComponentModel.CancelEventHandler(this.txtLadoB_Validating);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(124, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 46);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lado B";
+            // 
+            // txtLadoC
+            // 
+            this.txtLadoC.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLadoC.Location = new System.Drawing.Point(293, 208);
+            this.txtLadoC.Name = "txtLadoC";
+            this.txtLadoC.Size = new System.Drawing.Size(184, 53);
+            this.txtLadoC.TabIndex = 6;
+            this.txtLadoC.Validating += new System.ComponentModel.CancelEventHandler(this.txtLadoC_Validating);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(124, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 46);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Lado C";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.AllowDrop = true;
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(309, 301);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(168, 52);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.AllowDrop = true;
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(309, 379);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(168, 45);
+            this.btnSair.TabIndex = 8;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtLadoC);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLadoB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLadoA);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTestar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTestar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLadoA;
+        private System.Windows.Forms.TextBox txtLadoB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLadoC;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Atividade4/Atividade-Triângulo/Atividade-Triângulo/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade_Triângulo
+{
+    public partial class Form1 : Form
+    {
+        double ladoA, ladoB, ladoC;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtLadoA_TextChanged(object sender, EventArgs e)
+        {
+                        
+        }
+
+        private void txtLadoA_Validating(object sender, CancelEventArgs e)
+        {
+            if (txtLadoA.Text != string.Empty)
+            {
+                if (!double.TryParse(txtLadoA.Text, out ladoA))
+                    MessageBox.Show("Inserir um número válido");
+            }
+            else
+                MessageBox.Show("Inserir valor");
+
+        }
+
+        private void txtLadoB_Validating(object sender, CancelEventArgs e)
+        {
+            if (txtLadoB.Text != string.Empty)
+            {
+                if (!double.TryParse(txtLadoB.Text, out ladoB))
+                    MessageBox.Show("Inserir um número válido");
+            }
+            else
+                MessageBox.Show("Inserir valor");
+
+        }
+
+        private void txtLadoC_Validating(object sender, CancelEventArgs e)
+        {
+            if (txtLadoC.Text != string.Empty)
+            {
+                if (!double.TryParse(txtLadoC.Text, out ladoC))
+                    MessageBox.Show("Inserir um número válido");
+            }
+            else
+                MessageBox.Show("Inserir valor");
+
+        }
+
+        private void btnTestar_Click(object sender, EventArgs e)
+        {
+            if (ladoA < (ladoB+ladoC) && ladoA > Math.Abs(ladoB-ladoC) && ladoB < ladoA + ladoC && ladoB > Math.Abs (ladoA - ladoC) && ladoC < ladoA + ladoB && ladoC > Math.Abs (ladoA-ladoB))
+                {
+                if (ladoA == ladoB && ladoA == ladoC)
+                    MessageBox.Show("Triângulo equilátero");
+                else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+                    MessageBox.Show("Triângulo isóceles");
+                else MessageBox.Show("Triângulo escaleno");
+            }
+            else MessageBox.Show("Valores inválidos para um triângulo");
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtLadoA.Clear();
+            txtLadoB.Clear();
+            txtLadoC.Clear();
+        }
+    }
+}
